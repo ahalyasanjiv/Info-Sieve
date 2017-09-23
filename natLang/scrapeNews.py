2908fec78d3025e299683fd7a465c865842f6993
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2017-09-23 13:59:01 -0400

    Scraped data from Google News Headlines

--- natLang/scrapeNews.py
+++ natLang/scrapeNews.py
@@ -1,1 +1,21 @@
-Hi
+from bs4 import BeautifulSoup
+import urllib.request
+import csv
+
+def getNewsHeadlines(url):
+	markup = urllib.request.urlopen(url).read()
+	soup = BeautifulSoup(markup, "lxml")
+	headlines = [element.title.text for element in soup.findAll('item')][:10]
+	return headlines
+
+def getTopicUrl(topic):
+	with open('data/newsUrls.csv', mode='r') as infile:
+		reader = csv.reader(infile)
+		topicUrls = {row[0].replace('\ufeff', ''):row[1] for row in reader}
+	if topic in topicUrls:
+		return topicUrls[topic]
+
+if __name__ == '__main__':
+	url = getTopicUrl('World')
+	print(getNewsHeadlines(url))
+
