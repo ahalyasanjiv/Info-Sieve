a78b4bac5faf15bc2c13180f84f020ff8b4eb7e2
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2017-09-24 08:05:19 -0400

    Remove SDK

--- natLang/scrapeNews.py
+++ natLang/scrapeNews.py
@@ -1,21 +1,53 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
+import requests
+import feedparser
 
+"""
+Retrieves corresponding Google News URL for a given news category
+
+:param category: Selected news category  
+:returns: URL string for category
+"""
+def getCategoryUrl(category):
+	with open('data/newsUrls.csv', mode='r') as infile:
+		reader = csv.reader(infile)
+		categoryUrls = {row[0].replace('\ufeff', '') : row[1] for row in reader}
+	if category in categoryUrls:
+		return categoryUrls[category]
+
+"""
+Gives the top ten headlines for a news category given the url
+
+:param url: URL of category page in Google News
+:returns: List of headlines for news category
+"""
 def getNewsHeadlines(url):
 	markup = urllib.request.urlopen(url).read()
 	soup = BeautifulSoup(markup, "lxml")
 	headlines = [element.title.text for element in soup.findAll('item')][:10]
 	return headlines
 
-def getTopicUrl(topic):
-	with open('data/newsUrls.csv', mode='r') as infile:
-		reader = csv.reader(infile)
-		topicUrls = {row[0].replace('\ufeff', '') : row[1] for row in reader}
-	if topic in topicUrls:
-		return topicUrls[topic]
+
+"""
+Gets the links to the first n news results for the query
+
+:param query: The search request to be made
+:returns: List of links for the first n results when searching the query
+"""
+def getFirstNLinks(url,n):
+	d = feedparser.parse(url)
+	links = []
+	for i in range(5):
+		if (d.entries[i]!=''):
+			links.append(d.entries[i].link)
+	return links
 
 if __name__ == '__main__':
-	url = getTopicUrl('World')
-	print(getNewsHeadlines(url))
+	url = getCategoryUrl('World')
+	#print(getNewsHeadlines(url))
+	print(getFirstNLinks(url,1))
+
+
 
